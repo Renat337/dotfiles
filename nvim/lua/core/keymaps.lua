78c11546a02112d87335768f7a703737ab019dad
Renat Samur <renat.ren@yahoo.com>
2026-06-21 18:25:17 +0100

    Some quality of life keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,3 +1,5 @@
+local opts = { noremap = true, silent = true }
+
 vim.g.mapleader = " "
 
 -- vim.keymap.set({"n", "v"}, "<leader>y", '"+y')
@@ -37,3 +39,9 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
+
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
