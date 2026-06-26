9d602176982b6a944f4e21ed9584d2f4459c750b
Renat Samur <rsamur@amazon.com>
2026-06-26 14:28:28 +0000

    Adding better git hunk movement with gitsigns

--- nvim/lua/plugins/gitblame.lua
+++ nvim/lua/plugins/gitblame.lua
@@ -10,9 +10,11 @@ return {
                 vim.keymap.set(mode, l, r, opts)
             end
 
-            map("n", "]h", function() gs.nav_hunk("next") end, { desc = "Next git hunk" })
-            map("n", "[h", function() gs.nav_hunk("prev") end, { desc = "Previous git hunk" })
+            map("n", "]h", function() gs.nav_hunk("next", { target = "all" }) end, { desc = "Next git hunk" })
+            map("n", "[h", function() gs.nav_hunk("prev", { target = "all" }) end, { desc = "Previous git hunk" })
             map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, { desc = "Git blame line" })
+            map("n", "<leader>gs", gs.stage_hunk, { desc = "Git stage hunk" })
+            map("n", "<leader>gr", gs.reset_hunk, { desc = "Git reset hunk" })
             map("n", "<leader>gp", gs.preview_hunk, { desc = "Git preview hunk" })
             map("n", "<leader>gd", gs.diffthis, { desc = "Git diff this file" })
         end,
