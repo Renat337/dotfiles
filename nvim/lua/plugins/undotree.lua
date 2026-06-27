3cfe3ce5e0c73cbf0ea78012cc4862cfd70e7b7a
Renat Samur <renat.ren@yahoo.com>
2026-06-27 22:19:35 +0100

    Adding undotree

---
+++ nvim/lua/plugins/undotree.lua
@@ -0,0 +1,11 @@
+return {
+    "mbbill/undotree",
+
+    config = function()
+        vim.g.undotree_WindowLayout = 3
+
+        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, {
+            desc = "Toggle UndoTree",
+        })
+    end,
+}
